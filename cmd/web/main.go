9e8d198a73e673ee149aae3931d957f228c8b128
Jon Balon <development@jonbalon.com>
2022-06-08 15:06:53 -0400

    Listen and Serve on Port 9000

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	port := 9000
+	fmt.Printf("Server started and listening on port: %v\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
