a88eb88fa38a61828be14a5d8500ad7bd58a709d
Jon Balon <development@jonbalon.com>
2022-06-14 20:13:16 -0400

    `CreateTodoList` Model Uses MongoDB

--- models/list.go
+++ models/list.go
@@ -1,40 +1,37 @@
 package models
 
 import (
+	"context"
 	"errors"
 
 	"github.com/Jon1701/golang-todolist-app/config"
-	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type TodoList struct {
-	ID    *string        `json:"id,omitempty"`
-	Name  *string        `json:"name,omitempty"`
-	Items []TodoListItem `json:"items,omitempty"`
+	ID    *string        `json:"id,omitempty" bson:"_id,omitempty"`
+	Name  *string        `json:"name,omitempty" bson:"name,omitempty"`
+	Items []TodoListItem `json:"items,omitempty" bson:"items,omitempty"`
 }
 
 type TodoListItem struct {
-	Description *string `json:"description,omitempty"`
-	IsComplete  *bool   `json:"isComplete,omitempty"`
+	Description *string `json:"description,omitempty" bson:"description,omitempty"`
+	IsComplete  *bool   `json:"isComplete,omitempty" bson:"isComplete,omitempty"`
 }
 
 // Fake empty database.
 var db = []TodoList{}
 var client *mongo.Client
+var coll *mongo.Collection
 
 func init() {
 	config.Connect()
 	client = config.GetDB()
+	coll = client.Database("todolist").Collection("lists")
 }
 
-func (l *TodoList) CreateTodoList() (*TodoList, error) {
-	// Create new UUID.
-	id := uuid.NewString()
-
-	// Attach id to the list to be persisted.
-	l.ID = &id
-
+func (l *TodoList) CreateTodoList() (*string, error) {
 	// Iterate over the Todo List Items
 	for index := 0; index < len(l.Items); index++ {
 		// Explicitly set IsComplete status.
@@ -44,9 +41,16 @@ func (l *TodoList) CreateTodoList() (*TodoList, error) {
 		}
 	}
 
-	db = append(db, *l)
+	// Insert document.
+	result, err := coll.InsertOne(context.TODO(), l)
+	if err != nil {
+		return nil, err
+	}
 
-	return l, nil
+	// Get string representation of the Object ID.
+	id := result.InsertedID.(primitive.ObjectID).Hex()
+
+	return &id, nil
 }
 
 func GetTodoLists() []TodoList {
