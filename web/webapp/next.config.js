5f247295a562244e11cb83f1331df70752a9d26e
Jon Balon <development@jonbalon.com>
2022-06-16 20:34:54 -0400

    Next.js Configuration Proxy API Requests

--- web/webapp/next.config.js
+++ web/webapp/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: () => [
+    { source: '/api/:path*', destination: 'http://localhost:9000/api/:path*' }
+  ]
 }
 
 module.exports = nextConfig
