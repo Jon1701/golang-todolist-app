2b426f5c2c99f0f93468c838ad239f5c73772fe4
Jon Balon <development@jonbalon.com>
2022-06-22 12:48:01 -0400

    Main Page

--- web/webapp/pages/index.tsx
+++ web/webapp/pages/index.tsx
@@ -1,16 +1,127 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
-const Home: React.FC = () => {
+import { getTodoLists, ResponseCodes } from "@fetch/list";
+import { TodoList } from "@interfaces/TodoList";
+import DisplayLists from "@components/DisplayLists";
+import Panel from "@components/Panel";
+import LoadingSpinner from "@components/LoadingSpinner";
+
+enum ContentCodes {
+  Loading = "LOADING",
+  Success = "SUCCESS",
+  NoData = "NO_DATA",
+  UnknownError = "UNKNOWN_ERROR",
+}
+
+/**
+ * Container for the page.
+ */
+const Container = styled.main`
+  min-height: 100vh;
+`;
+
+/**
+ * Container for the content.
+ */
+const Content = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 25px 0;
+`;
+
+/**
+ * Custom H1.
+ */
+const H1 = styled.h1`
+  margin-top: 0;
+`;
+
+/**
+ * Container for error messages.
+ */
+const ContainerErrorMessage = styled.div`
+  text-align: center;
+`;
+
+/**
+ * Index page.
+ *
+ * @returns Index page.
+ */
+const IndexPage: React.FC = () => {
+  // Controls the content to be displayed.
+  const [contentCode, setContentCode] = useState<ContentCodes>(
+    ContentCodes.Loading
+  );
+
+  // Todolist data.
+  const [data, setData] = useState<Array<TodoList> | undefined>(undefined);
+
+  /**
+   * Fetches TodoLists, saves data in local state, sets content visibility.
+   */
   const fetchTodoLists = async () => {
-    const result = await fetch("/api/list", { method: "GET" });
-    console.log(result);
+    setContentCode(ContentCodes.Loading);
+    setData([]);
+
+    const res = await getTodoLists();
+    switch (res.responseCode) {
+      case ResponseCodes.NoData:
+        setContentCode(ContentCodes.NoData);
+        break;
+
+      case ResponseCodes.Success:
+        setData(res.body);
+        setContentCode(ContentCodes.Success);
+        break;
+
+      default:
+        setContentCode(ContentCodes.UnknownError);
+        break;
+    }
   };
 
   useEffect(() => {
-    fetchTodoLists();
+    void fetchTodoLists();
   }, []);
 
-  return <div>Hello World</div>;
+  /**
+   * Renders components which correspond to a given content code.
+   *
+   * @param code Content code.
+   * @returns Components to be rendered.
+   */
+  const renderSwitch: React.FC<ContentCodes> = (code) => {
+    switch (code) {
+      case ContentCodes.Loading:
+        return <LoadingSpinner />;
+
+      case ContentCodes.Success:
+      case ContentCodes.NoData:
+        return (
+          <React.Fragment>
+            <H1>My Todo Lists</H1>
+            {data ? <DisplayLists data={data} /> : null}
+          </React.Fragment>
+        );
+
+      default:
+        return (
+          <ContainerErrorMessage>
+            An unknown error occurred.
+          </ContainerErrorMessage>
+        );
+    }
+  };
+
+  return (
+    <Container>
+      <Content>
+        <Panel>{renderSwitch(contentCode)}</Panel>
+      </Content>
+    </Container>
+  );
 };
 
-export default Home;
+export default IndexPage;
