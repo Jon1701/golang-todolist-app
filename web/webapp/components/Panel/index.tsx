1d6fe77bc047bda7631650a87ffe4ef8c115bd8b
Jon Balon <development@jonbalon.com>
2022-06-22 02:19:41 -0400

    `<Panel />` Component

---
+++ web/webapp/components/Panel/index.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+/**
+ * Panel component.
+ */
+const Panel = styled.div`
+  background-color: #fff;
+
+  border-radius: 8px;
+  padding: 10px;
+`;
+
+export default Panel;
