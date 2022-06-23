b801a159992c0708c039c9b23fe2a672e6c0541b
Jon Balon <development@jonbalon.com>
2022-06-23 16:46:44 -0400

    `TodoList` Reducer with `SET_ITEM_DESCRIPTION` Action

--- web/webapp/components/TodoListForm/useReducer/reducers.ts
+++ web/webapp/components/TodoListForm/useReducer/reducers.ts
@@ -1,5 +1,5 @@
 import { ActionTypes, ActionCreator } from "./actions";
-import { TodoList } from "@interfaces/TodoList";
+import { TodoList, TodoListItem } from "@interfaces/TodoList";
 
 /**
  * Modifies the TodoList state.
@@ -13,10 +13,28 @@ const todoListReducer = (state: TodoList, action: ActionCreator) => {
     case ActionTypes.SetName:
       return { ...state, name: action.payload };
 
-    case ActionTypes.SetNewBlankItem:
+    case ActionTypes.SetNewBlankItem: {
       const items = state.items?.slice() || [];
       items.push(action.payload);
       return { ...state, items };
+    }
+
+    case ActionTypes.SetItemDescription:
+      {
+        // Create copy of items array.
+        const items = state.items.slice();
+
+        // Get index of the given TodoList item in the items array.
+        const idx: number = items.findIndex(
+          (currentItem: TodoListItem) => currentItem.id === action.payload.id
+        );
+
+        items[idx].description = action.payload.description;
+
+        return { ...state, items };
+      }
+
+      return;
 
     default:
       return state;
