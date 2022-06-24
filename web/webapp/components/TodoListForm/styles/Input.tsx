e9395d52576144410d2e05dd19d8eda11bfdfe90
Jon Balon <development@jonbalon.com>
2022-06-24 15:31:47 -0400

    `TodoListForm` `<Input />` Component Has Red Border with Validation Message

--- web/webapp/components/TodoListForm/styles/Input.tsx
+++ web/webapp/components/TodoListForm/styles/Input.tsx
@@ -1,20 +1,30 @@
 import styled from "styled-components";
 
+interface StyleProps {
+  /**
+   * Field validation message.
+   */
+  validationMessage?: string;
+}
+
 /**
  * Styled input.
  */
-const Input = styled.input`
+const Input = styled.input<StyleProps>`
   &[type="text"] {
     display: block;
     width: 100%;
     box-sizing: border-box;
 
     font-size: 1rem;
+    background-color: #f6f6f6;
+
     border-radius: 0px;
     padding: 5px;
 
-    border: none;
-    background-color: #f6f6f6;
+    border-style: solid;
+    border-width: 1px;
+    border-color: ${(props) => (!props.validationMessage ? "none" : "#dc143c")};
   }
 `;
 
