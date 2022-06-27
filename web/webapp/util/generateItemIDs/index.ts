5cb69960c2966080e4fb57803d70ab644c33d951
Jon Balon <development@jonbalon.com>
2022-06-27 16:25:36 -0400

    Utility Function to Generate IDs for TodoList Items

---
+++ web/webapp/util/generateItemIDs/index.ts
@@ -0,0 +1,25 @@
+import { TodoList } from "@interfaces/TodoList";
+import { v4 as uuidv4 } from "uuid";
+
+/**
+ * Generate IDs for items in a TodoList.
+ *
+ * @param formValues Todo List.
+ * @returns Todo List with IDs for each item.
+ */
+const generateItemIDs: TodoList = (formValues: TodoList) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const copy: TodoList = JSON.parse(JSON.stringify(formValues));
+
+  if (copy.items !== undefined && copy.items.length > 0) {
+    copy.items = copy.items.map((item) => {
+      item.id = uuidv4();
+
+      return item;
+    });
+  }
+
+  return copy;
+};
+
+export default generateItemIDs;
